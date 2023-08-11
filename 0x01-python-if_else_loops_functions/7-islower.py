5140ae3a864a212aed6bd63409da82d695a403ce
Bashiru Ayomide <thenetcompany1@gmail.com>
2023-08-11 05:49:34 +0100

    HOT FIX

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-digit1 = 0
-digit2 = 1
-while digit1 <= 8:
-    while digit2 <= 9:
-        if digit1 != digit2:
-            print("{:d}".format(digit1), end='')
-            if digit1 != 8:
-                print("{:d}, ".format(digit2), end='')
-            else:
-                print("{:d}".format(digit2))
-        digit2 += 1
-    digit2 = digit1 + 1
-    digit1 = digit1 + 1
+# 7-islower.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+
+def islower(c):
+    """Check for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
