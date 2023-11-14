755b55bfd604e6551288d3f7625d2d024b8a13d3
Bashiru Ayomide <thenetcompany1@gmail.com>
2023-11-14 05:58:15 +0100

    permisson fix

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ 0x0A. Python - Inheritance, task 8 """
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
@@ -20,3 +22,22 @@ class Rectangle(BaseGeometry):
         self.__width = width
         super().integer_validator("height", height)
         self.__height = height
+
+    def area(self):
+        """Returns rectangle area as width * height.
+
+        Attributes:
+            __width (int): x dimension of rectangle
+            __height (int): y dimension of rectangle
+
+        Returns:
+            __width * __height
+
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ Provides printable representation of Rectangle object.
+
+        """
+        return '[Rectangle] {}/{}'.format(self.__width, self.__height)
